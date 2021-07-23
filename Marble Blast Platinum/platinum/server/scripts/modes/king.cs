e36b6691198b5f1dcfe6f9fb4146c438b50108a0
main-gi <43479839+main-gi@users.noreply.github.com>
2021-07-23 04:58:35 -0400

    Display gem message and PhysMod corner triggers
    My vision for this mode: blast near enemies somehow to steal and move the king trigger elsewhere.

--- Marble Blast Platinum/platinum/server/scripts/modes/king.cs
+++ Marble Blast Platinum/platinum/server/scripts/modes/king.cs
@@ -23,7 +23,9 @@
 //-----------------------------------------------------------------------------
 
 function Mode_king::onLoad(%this) {
+	%this.registerCallback("onMissionLoaded");
 	%this.registerCallback("onFoundGem");
+	%this.registerCallback("shouldStoreGem");
 	%this.registerCallback("getStartTime");
 	%this.registerCallback("shouldResetTime");
 	%this.registerCallback("shouldRestartOnOOB");
@@ -31,8 +33,94 @@ function Mode_king::onLoad(%this) {
 	%this.registerCallback("timeMultiplier");
 	echo("[Mode" SPC %this.name @ "]: Loaded!");
 }
+function Mode_king::onMissionLoaded(%this) {
+	// Make triggers for the King stuff
+	buildKingEmitters(MissionGroup);
+}
+
+function buildKingEmitters(%group) {
+	%count = %group.getCount();
+
+	for (%i = 0; %i < %count; %i++) {
+		%obj = %group.getObject(%i);
+		%class = %obj.getClassName();
+
+		if (%class $= "SimGroup")
+			buildKing(%obj);
+		else if (%class $= "Trigger" &&
+		         %obj.getDataBlock().getName() $= "KingTrigger" &&
+		         !%obj.noEmitters) {
+
+			if (%obj._builtEmitters) {
+				continue;
+			}
+			%obj._builtEmitters = true;
+
+			// create position array for each emitter to be placed around
+			// the corner of the physmod trigger. I mean king trigger.
+			%pos = %obj.getTransform();
+			%scale = %obj.getScale();
+			%x = getWord(%pos, 0);
+			%y = getWord(%pos, 1);
+			%z = getWord(%pos, 2);
+			%sX = getWord(%scale, 0);
+			%sY = getWord(%scale, 1);
+			%sZ = getWord(%scale, 2);
+
+			%pos[0] = %x SPC %y SPC %z;
+			%pos[1] = %x + %sX SPC %y SPC %z;
+			%pos[2] = %x SPC %y - %sY SPC %z;
+			%pos[3] = %x + %sX SPC %y - %sY SPC %z;
+			%pos[4] = %x SPC %y SPC %z + %sZ;
+			%pos[5] = %x + %sX SPC %y SPC %z + %sZ;
+			%pos[6] = %x SPC %y - %sY SPC %z + %sZ;
+			%pos[7] = %x + %sX SPC %y - %sY SPC %z + %sZ;
+			// Draw physmod shape on each corner
+			for (%j = 0; %j <= 3; %j++) {
+				%staticShape = new StaticShape() {
+					position = %pos[%j];
+					rotation = "1 0 0 0";
+					scale = "1 1 1";
+					datablock = PhysModEmitterBase;
+				};
+				MissionCleanup.add(%staticShape);
+				$PhysModStaticShape[%obj.getId()] = %staticShape;
+			}
+			//%staticShape = new StaticShape() {
+			//	position = %pos[0];
+			//	rotation = "1 0 0 0";
+			//	scale = %sX SPC %sY SPC %sZ;
+			//	dataBlock = "WaterCylinder";
+			//};
+			//MissionCleanup.add(%staticShape);
+			//$PhysModStaticShape[%obj.getId()] = %staticShape;
+
+			// Draw 6 rectangles that correspond to the king trigger
+			//%rectpos[0] = %pos[0] SPC %pos[1] SPC %pos[2];
+			//%rectsize[0] = %sX/8 SPC %sY/8 SPC 0.001;
+			//%rectpos[1] = %pos[4] SPC %pos[5] SPC %pos[6];
+			//%rectsize[1] = %sX/8 SPC %sY/8 SPC 0.001;
+			//%rectpos[2] = %pos[0] SPC %pos[1] SPC %pos[4];
+			//%rectsize[2] = 0.001 SPC %sY/8 SPC %sZ/8;
+			//for (%j = 0; %j <= 2; %j++) {
+			//	%staticShape = new StaticShape() {
+			//		position = %rectpos[%j];
+			//		rotation = "1 0 0 0";
+			//		scale = %rectsize[%j];
+			//		dataBlock = "WaterPlane";
+			//	};
+			//}
+		}
+	}
+}
+
 function Mode_king::onFoundGem(%this, %object) {
 //	%object.client.playPitchedSound("gotDiamond");
+	%object.client.displayGemMessage("+1", "ff0000");
+	return false;
+}
+
+function Mode_king::shouldStoreGem(%this, %object) {
 }
 function Mode_king::getStartTime(%this) {
 	return (MissionInfo.time ? MissionInfo.time : 300000);
