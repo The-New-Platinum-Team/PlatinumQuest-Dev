48dea0182891bd4e260c673924b23ca2c8c8d618
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2021-08-05 15:40:50 +0530

    mbu gravmod shader test

--- Marble Blast Platinum/platinum/data/shaders/shapeF.glsl
+++ Marble Blast Platinum/platinum/data/shaders/shapeF.glsl
@@ -1,4 +1,5 @@
 //-----------------------------------------------------------------------------
+// Copyright (c) 2020 Whirligig231
 // Copyright (c) 2021 The Platinum Team
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,12 +21,38 @@
 // DEALINGS IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
 
-varying vec2 UV;
-uniform sampler2D textureSampler;
-uniform vec2 screenSize;
+varying vec2 uv;
+varying vec3 vert_position;
+varying vec3 normal_vector;
 
-float roundN(float x, float y) {
-    return floor(x / y + 0.5) * y;
+uniform sampler2D textureSampler;
+// uniform sampler2D normalSampler;
+#if (QUALITY_LEVEL > 0)
+// uniform sampler2D specularSampler;
+#endif
+uniform samplerCube skyboxSampler;
+
+uniform vec3 camera_position;
+uniform mat4 rot_from_torque_mat;
+uniform mat4 rot_to_torque_mat;
+uniform vec4 ambient_color;
+uniform vec4 sun_color;
+uniform vec3 sun_direction;
+
+float tanh(float x) {
+    return (exp(2.0 * x) - 1.0) / (exp(2.0 * x) + 1.0);
+}
+
+float sigmoid(float x) {
+    return 0.5 + 0.5 * tanh(2.0 * x - 1.0);
+}
+
+float atanh(float x) {
+    return 0.5 * log((1.0 + x) / (1.0 - x));
+}
+
+float invSigmoid(float x) {
+    return 0.5 + 0.5 * atanh(2.0 * x - 1.0);
 }
 
 // Hardcoded through automation
@@ -439,49 +466,53 @@ int checkGlowColor(vec3 color) {
 }
 
 void main() {
-    vec2 pixel = (UV * screenSize);
+    // Correct UV based on texture scale
+    vec2 scaled_uv = uv;
 
-    vec4 color = (texture2D(textureSampler, pixel / screenSize));
+    // Texture values
+    vec4 material_color = texture2D(textureSampler, scaled_uv);
 
-    const int BLOCK_SIZE = 8;
-
-    const float RADIUS_OFFSET = 1.5;
-
-    // Calc luminance for 8x8 pixel block 
-    vec4 lumVector[BLOCK_SIZE * BLOCK_SIZE];
-
-    int count;
-    for(int i = 0; i < BLOCK_SIZE; i++) {
-        for(int j = 0; j < BLOCK_SIZE; j++) {
-
-            vec2 offset = vec2(float(i * RADIUS_OFFSET), float(j * RADIUS_OFFSET));
-            vec2 centre = vec2(BLOCK_SIZE * RADIUS_OFFSET / 2, BLOCK_SIZE * RADIUS_OFFSET / 2);
-
-            vec4 c = texture2D(textureSampler, (pixel + offset - centre) / screenSize);
-
-            float luminance = 0;
-
-            if(checkGlowColor(c.rgb) == 1) {
-                luminance = 1;
-            }
-            vec4 brightColor = c * 2;
-
-            brightColor.xyz *= sign(luminance);
-            brightColor.a = 1.0;
-
-            lumVector[i * BLOCK_SIZE + j] = brightColor;
-            count++;
-        }
+    if(checkGlowColor(material_color.rgb) == 1) {
+        gl_FragColor = vec4(material_color.rgb, 1);
+        return;
     }
 
-    vec4 avgLum = vec4(0, 0, 0, 0);
-    for(int i = 0; i < BLOCK_SIZE; i++) {
-        for(int j = 0; j < BLOCK_SIZE; j++) {
-            avgLum += lumVector[i * BLOCK_SIZE + j];
-        }
-    }
-    avgLum /= count;
+    // Normalize the light vector so we can dot it
+    vec3 light_normal = normalize(-sun_direction);
 
-    // Invert colors
-    gl_FragColor = color + avgLum;
+    // Cosine of the angle from the light to the normal
+    float cosTheta = clamp(dot(normal_vector, light_normal), 0, 1);
+
+    // Sun color is multiplied by angle for bump mapping, then clamped to [0, 1] so we don't clip
+    vec4 effectiveSun = sun_color * cosTheta;
+
+    // Ambient color first
+    effectiveSun += ambient_color;
+
+    // Clamp sun so we don't clip
+    effectiveSun = vec4(clamp(effectiveSun.r, 0, 1), clamp(effectiveSun.g, 0, 1), clamp(effectiveSun.b, 0, 1), 1);
+
+    // Worldspace normal taking normal mapping into account
+    vec3 normal_model = normal_vector;  // normalize(reflect(-normal_vector, normal_color));
+    // Direction from camera to vertex
+    vec3 camera_direction = normalize(vert_position - camera_position);
+    // Reflect the camera off the normal so we know where on the skysphere to show
+    vec3 camera_reflection = reflect(camera_direction, normal_model);
+
+    // Reflected coordinates onto the given skybox
+    vec3 skyboxR = camera_reflection;
+    // Get the color from the skybox
+    vec4 reflectionColor = textureCube(skyboxSampler, skyboxR);
+    // Apply the reflected skybox color
+    float reflectAmount = invSigmoid(0.01 + 0.98 * material_color.a);
+    reflectAmount -= 0.7 * (2.0 * -dot(normal_model, camera_direction) - 1.0);
+    reflectAmount = sigmoid(reflectAmount);
+    reflectAmount = 0.95 * reflectAmount;
+
+    vec4 firstColor = vec4(material_color.rgb * effectiveSun.rgb, 1);
+
+    vec4 color = texture2D(textureSampler, scaled_uv);
+
+    gl_FragColor = firstColor;
+    // gl_FragColor = vec4((1+normal_model)/2, 0);
 }
