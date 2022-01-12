a913eef4d0704665018a4ac88ec34c0738b6d7ef
daniel741852 <danielhernandez175@gmail.com>
2022-01-12 03:14:32 -0700

    [EXPERIMENTAL] Automatic UI Option Implemented missiondownload.cs optimized MBP Timer texturepack Implemented

--- Marble Blast Platinum/platinum/client/defaults.cs
+++ Marble Blast Platinum/platinum/client/defaults.cs
@@ -127,6 +127,7 @@ $pref::Music::Songs["Game"]   = "*";
 //Graphics
 $pref::Snore = true;
 $pref::AnimatePreviews = true;
+$pref::AutomaticUI = false;
 $pref::LegacyItems = false;
 $pref::ProfanityFilter = 2;
 $pref::DefaultSkybox = "platinum/data/skies_pq/Blender3/blender3.dml";
