6413af6d80c8a3cb6bbb7aa01809d7dc20f83e5f
daniel741852 <danielhernandez175@gmail.com>
2022-04-28 20:36:39 -0400

    MBG customs texpack fix using swapTextures instead of Texture Packs. This results in more optimal load times.

--- Marble Blast Platinum/platinum/client/scripts/missiondownload.cs
+++ Marble Blast Platinum/platinum/client/scripts/missiondownload.cs
@@ -160,13 +160,51 @@ function onPhase3Complete() {
 // Start of MBG Custom Textures Patch
 
 function mbgTexPatchActivate() {
-	activateTexturePack("mbgcustomspatch");
-	reloadTexturePacks();
+	swapTextures("platinum/data/interiors/custom_woodblockside", "platinum/data/interiors_mbg/custom_woodblockside");
+	swapTextures("platinum/data/interiors/custom_woodblocktop", "platinum/data/interiors_mbg/custom_woodblocktop");
+	swapTextures("platinum/data/interiors/edge_cool1", "platinum/data/interiors_mbg/edge_cool1");
+	swapTextures("platinum/data/interiors/edge_cool2", "platinum/data/interiors_mbg/edge_cool2");
+	swapTextures("platinum/data/interiors/edge_neutral1", "platinum/data/interiors_mbg/edge_neutral1");
+	swapTextures("platinum/data/interiors/edge_neutral2", "platinum/data/interiors_mbg/edge_neutral2");
+	swapTextures("platinum/data/interiors/edge_warm1", "platinum/data/interiors_mbg/edge_warm1");
+	swapTextures("platinum/data/interiors/edge_warm2", "platinum/data/interiors_mbg/edge_warm2");
+	swapTextures("platinum/data/interiors/edge_white", "platinum/data/interiors_mbg/edge_white");
+	swapTextures("platinum/data/interiors/grid_cool2", "platinum/data/interiors_mbg/grid_cool2");
+	swapTextures("platinum/data/interiors/grid_neutral", "platinum/data/interiors_mbg/grid_neutral");
+	swapTextures("platinum/data/interiors/grid_neutral1", "platinum/data/interiors_mbg/grid_neutral1");
+	swapTextures("platinum/data/interiors/grid_neutral2", "platinum/data/interiors_mbg/grid_neutral2");
+	swapTextures("platinum/data/interiors/grid_neutral4", "platinum/data/interiors_mbg/grid_neutral4");
+	swapTextures("platinum/data/interiors/grid_warm", "platinum/data/interiors_mbg/grid_warm");
+	swapTextures("platinum/data/interiors/grid_warm1", "platinum/data/interiors_mbg/grid_warm1");
+	swapTextures("platinum/data/interiors/stripe_caution", "platinum/data/interiors_mbg/stripe_caution");
+	swapTextures("platinum/data/interiors/tube_cool", "platinum/data/interiors_mbg/tube_cool");
+	swapTextures("platinum/data/interiors/tube_neutral", "platinum/data/interiors_mbg/tube_neutral");
+	swapTextures("platinum/data/interiors/tube_warm", "platinum/data/interiors_mbg/tube_warm");
+	swapTextures("platinum/data/interiors/edge_white2", "platinum/data/interiors_mbg/edge_white2");
 }
 
 function mbgTexPatchDeactivate() {
-	deactivateTexturePack("mbgcustomspatch");
-	reloadTexturePacks();
+	swapTextures("platinum/data/interiors/custom_woodblockside", "platinum/data/interiors/custom_woodblockside");
+	swapTextures("platinum/data/interiors/custom_woodblocktop", "platinum/data/interiors/custom_woodblocktop");
+	swapTextures("platinum/data/interiors/edge_cool1", "platinum/data/interiors/edge_cool1");
+	swapTextures("platinum/data/interiors/edge_cool2", "platinum/data/interiors/edge_cool2");
+	swapTextures("platinum/data/interiors/edge_neutral1", "platinum/data/interiors/edge_neutral1");
+	swapTextures("platinum/data/interiors/edge_neutral2", "platinum/data/interiors/edge_neutral2");
+	swapTextures("platinum/data/interiors/edge_warm1", "platinum/data/interiors/edge_warm1");
+	swapTextures("platinum/data/interiors/edge_warm2", "platinum/data/interiors/edge_warm2");
+	swapTextures("platinum/data/interiors/edge_white", "platinum/data/interiors/edge_white");
+	swapTextures("platinum/data/interiors/grid_cool2", "platinum/data/interiors/grid_cool2");
+	swapTextures("platinum/data/interiors/grid_neutral", "platinum/data/interiors/grid_neutral");
+	swapTextures("platinum/data/interiors/grid_neutral1", "platinum/data/interiors/grid_neutral1");
+	swapTextures("platinum/data/interiors/grid_neutral2", "platinum/data/interiors/grid_neutral2");
+	swapTextures("platinum/data/interiors/grid_neutral4", "platinum/data/interiors/grid_neutral4");
+	swapTextures("platinum/data/interiors/grid_warm", "platinum/data/interiors/grid_warm");
+	swapTextures("platinum/data/interiors/grid_warm1", "platinum/data/interiors/grid_warm1");
+	swapTextures("platinum/data/interiors/stripe_caution", "platinum/data/interiors/stripe_caution");
+	swapTextures("platinum/data/interiors/tube_cool", "platinum/data/interiors/tube_cool");
+	swapTextures("platinum/data/interiors/tube_neutral", "platinum/data/interiors/tube_neutral");
+	swapTextures("platinum/data/interiors/tube_warm", "platinum/data/interiors/tube_warm");
+	swapTextures("platinum/data/interiors/edge_white2", "platinum/data/interiors/edge_white2");
 }
 
 // End of MBG Custom Textures Patch
