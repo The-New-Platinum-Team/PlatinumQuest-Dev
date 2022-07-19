1e2580327bf93d3cb0805470fef9a6c4626ea884
BlastedMarble <blastedmarble9b@gmail.com>
2022-07-19 16:27:30 -0700

    New level and other stuff for [REDACTED]

--- Marble Blast Platinum/platinum/client/scripts/radar.cs
+++ Marble Blast Platinum/platinum/client/scripts/radar.cs
@@ -488,6 +488,7 @@ function Radar::AddDot(%object, %bitmap) {
 			%name = strReplace(%name, "_MBU", "");
 			%name = strReplace(%name, "_MBM", "");
 			%name = strReplace(%name, "_MBXP", "");
+			%name = strReplace(%name, "_BM", "");
 			%skin = %object.getSkinName();
 			%bitmap = $userMods @ "/client/ui/mp/radar/" @ %name @ ".png";
 
