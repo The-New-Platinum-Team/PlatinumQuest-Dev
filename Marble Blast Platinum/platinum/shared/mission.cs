b73645c185ab751ec504e57c0dd704b87490f910
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-09-07 23:22:52 +0530

    fix broken gem counter in replays, smh

--- Marble Blast Platinum/platinum/shared/mission.cs
+++ Marble Blast Platinum/platinum/shared/mission.cs
@@ -471,8 +471,6 @@ function resolveMissionGameModes(%mission, %inputmodes) {
 		%modes = addWord(%modes, $Event::Modes);
 	}
 
-	echo(%mission, %mission.file);
-
 	%isChallenge = strPos(%info.file, "challenge") != -1;
 
 	if (%isChallenge && strPos(%modes, "challenge") == -1) {
