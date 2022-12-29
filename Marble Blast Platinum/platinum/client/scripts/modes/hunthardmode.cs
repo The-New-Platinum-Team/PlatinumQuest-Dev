3a7ce47a60859b6b402686991d4e4e653cdabda4
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-12-29 17:58:08 +0530

    Revert "Shortened the description for Hard Mode"
    This reverts commit b4aee432fdaa2b7c44d2d99735ce5f204ed1a958.

--- Marble Blast Platinum/platinum/client/scripts/modes/hunthardmode.cs
+++ Marble Blast Platinum/platinum/client/scripts/modes/hunthardmode.cs
@@ -30,7 +30,7 @@ ModeInfoGroup.add(new ScriptObject(ModeInfo_hunthardmode) {
 	file = "hunthardmode";
 
 	name = "Hunt - Hard Mode";
-	desc = "No PowerUps, No Blasting, No Yellows or Blues. Black Gems will give you a random Powerup.";
+	desc = "No PowerUps, No Blasting, No Yellows or Blues. Black Gems will give you a random Powerup. Good luck! (I do not recommend playing on maps with gems that are unreachable without PU's)";
 
 	teams = 1;
 });
