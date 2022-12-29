b2a16a0134b93413fc7fb614d4ccbf413530b9ce
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-12-29 17:58:12 +0530

    Revert "Maybe now fix the blasting issue with hard mode/also it popping up when the server hasnt activated it"
    This reverts commit a61e4c31c0526d33240f4d0ccbc9b7af9e93c0b5.

--- Marble Blast Platinum/platinum/shared/mission.cs
+++ Marble Blast Platinum/platinum/shared/mission.cs
@@ -520,7 +520,7 @@ function resolveMissionGameModes(%mission, %inputmodes) {
 			%modes = %modes SPC "competitive";
 		if (findWord(%modes, "hunt") != -1 && findWord(%modes, "coop") == -1 && ($MPPref::Server::PartySpawns || $MP::Client::ServerSetting["PartySpawns"]))
 			%modes = %modes SPC "partyspawns";
-		if (findWord(%modes, "hunt") != -1 && findWord(%modes, "coop") == -1 && $MPPref::Server::HuntHardMode)
+		if (findWord(%modes, "hunt") != -1 && findWord(%modes, "coop") == -1 && ($MPPref::Server::HuntHardMode || $MP::Client::ServerSetting["HuntHardMode"]))
 			%modes = %modes SPC "hunthardmode";
 	}
 
