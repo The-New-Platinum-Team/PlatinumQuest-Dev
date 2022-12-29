3c00eff4a4b2192349c722c77fd1e9fc5e0f6c83
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-12-29 18:06:02 +0530

    Revert "Competitive timer should not appear between levels"
    This reverts commit aa2a28b400a0e90c26b9b80f8c1d38d90ac67fbe.

--- Marble Blast Platinum/platinum/client/scripts/playGui.cs
+++ Marble Blast Platinum/platinum/client/scripts/playGui.cs
@@ -1096,10 +1096,6 @@ function PlayGui::updateTimeTravelCountdown(%this) {
 }
 
 function PlayGui::updateCountdownLeft(%this, %delta) {
-	PGCountdownLeft.setVisible(%this.runningCountdownLeft && %this.countdownLeftTime > 0);
-	if (!%this.runningCountdownLeft) {
-		return;
-	}
 	%this.countdownLeftTime = sub64_int(%this.countdownLeftTime, %delta);
 
 	%timeUsed = %this.countdownLeftTime + 99; // If the timer is say 30s, display 30.0 for its full amount instead of 29.9. Turns out adding 99 actually works perfectly here.
@@ -1145,6 +1141,7 @@ function PlayGui::updateCountdownLeft(%this, %delta) {
 	PGCountdownLeftPoint.setPosition((%secondsLeft >= 10 ? "403" : "388") + %leftOffset + %offsetIfThousandths SPC "0");
 
 	PGCountdownLeftSecondDigit.setVisible(%secondsLeft >= 10);
+	PGCountdownLeft.setVisible(%this.countdownLeftTime > 0);
 }
 
 function PlayGui::updateControls(%this) {
@@ -1347,7 +1344,6 @@ function PlayGui::startCountdown(%this, %time, %image) {
 
 function PlayGui::stopCountdown(%this) {
 	%this.runningCountdown = false;
-	%this.runningCountdownLeft = false;
 
 	if (%this.showingEggTime) {
 		%this.updateEggTime();
@@ -1413,5 +1409,4 @@ function PlayGui::updateCountdown(%this, %delta) {
 function PlayGui::startCountdownLeft(%this, %time, %image) {
 	PGCountdownLeftImage.setBitmap("platinum/client/ui/game/countdown/" @ %image);
 	%this.countdownLeftTime = %time;
-	%this.runningCountdownLeft = true;
 }
