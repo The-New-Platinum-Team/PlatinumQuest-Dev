ca319dde3f22aca4eb65ab54715e58a2454f4ae9
Terry Hearst <thearst3rd@gmail.com>
2023-02-28 02:08:38 -0500

    Fix SimObject attempt

--- Marble Blast Platinum/platinum/server/scripts/game.cs
+++ Marble Blast Platinum/platinum/server/scripts/game.cs
@@ -411,7 +411,7 @@ function startGame() {
 	onNextFrame(activateMovingObjects, true);
 
 	if (RtaSpeedrun.isEnabled) {
-		RtaSpeedrun.missionStarted();
+		RtaSpeedrun::missionStarted();
 	}
 }
 
@@ -437,7 +437,7 @@ function endGameSetup() {
 	serverSendCallback("onEndGameSetup");
 
 	if (RtaSpeedrun.isEnabled) {
-		RtaSpeedrun.missionEnded();
+		RtaSpeedrun::missionEnded();
 	}
 
 	if ($Server::ServerType $= "MultiPlayer") {
