05b1e9d6a3e2d1a1d43b2c4047dae2ad7ad36628
Terry Hearst <thearst3rd@gmail.com>
2023-03-21 00:14:31 -0400

    Add basic UI for RTA Speedrun mode

--- Marble Blast Platinum/platinum/client/init.cs
+++ Marble Blast Platinum/platinum/client/init.cs
@@ -147,6 +147,7 @@ function initClient() {
 	exec("./ui/EditorOrNewDlg.gui");
 	exec("./ui/ControllerGui.gui");
 	exec("./ui/QueueResultsDlg.gui");
+	exec("./ui/RtaSpeedrunDlg.gui");
 
 	// Gui Scripts
 	exec("./scripts/EndGameDlg.cs");
