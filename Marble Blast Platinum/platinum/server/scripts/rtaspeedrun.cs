6d78bff1ea51383ec3216d69110e42a8471884c1
Terry Hearst <thearst3rd@gmail.com>
2023-03-25 22:18:14 -0400

    Inform RTAAutosplitter if the game is paused or not

--- Marble Blast Platinum/platinum/server/scripts/rtaspeedrun.cs
+++ Marble Blast Platinum/platinum/server/scripts/rtaspeedrun.cs
@@ -125,6 +125,8 @@ function RtaSpeedrun::start(%this) {
 
 	%this.updateTimers();
 
+	RTAAS_setIsPauseScreenOpen(false);
+
 	echo("An RTA speedrun will start when you enter a level, and end when you finish" SPC %this.endMission);
 	echo("Good luck!");
 }
@@ -198,12 +200,14 @@ function RtaSpeedrun::missionEnded(%this) {
 }
 
 function RtaSpeedrun::pauseGame(%this) {
+	RTAAS_setIsPauseScreenOpen(true);
 	if (!%this.isEnabled)
 		return;
 	%this.pauseStartedTime = getRealTime();
 }
 
 function RtaSpeedrun::unpauseGame(%this) {
+	RTAAS_setIsPauseScreenOpen(false);
 	if (!%this.isEnabled)
 		return;
 	if (%this.pauseStartedTime < 0)
