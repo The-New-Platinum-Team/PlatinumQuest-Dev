aede3ae07248ed2569d501ad746c5fec5a8d7ab0
Connie Julie <alexnicolas155@yahoo.com>
2023-04-23 15:52:13 +0300

    Some changes to the King (KotH) Trigger (Check Desc of Commit)
    These changes are meant to make the King Trigger from the KotH game mode a bit more visible than it originally was.
    
    - Added King Trigger Emitters
    - Added Messages and Sounds for whenever a player enters and exits a King Trigger

--- Marble Blast Platinum/platinum/server/scripts/modes/king.cs
+++ Marble Blast Platinum/platinum/server/scripts/modes/king.cs
@@ -103,10 +103,123 @@ datablock TriggerData(KingTrigger) {
 	customField[0, "default"] = "1";
 };
 
+datablock StaticShapeData(KingTrigEmitterBase) {
+	fxDatablock[0] = "KingTrigEmitterNode";
+	fxEmitter[0] = "KingTrigEmitter";
+	fxSkin[0] = true;
+};
+
+function KingTrigEmitterBase::onAdd(%this, %obj) {
+	%this.initFX(%obj);
+}
+
+datablock ParticleData(KingTrigParticle) {
+	textureName          = "platinum/data/particles/orb";
+	dragCoefficient      = 1;
+	gravityCoefficient   = 0.2;
+	windCoefficient = 0;
+	inheritedVelFactor   = 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 200;
+	lifetimeVarianceMS   = 0;
+
+	colors[0]     = "0.99 0.54 0.09 1";
+	colors[1]     = "0.99 0.54 0.09 1";
+
+	sizes[0]      = 1;
+	sizes[1]      = 1;
+};
+
+datablock ParticleEmitterData(KingTrigEmitter) {
+	ejectionPeriodMS = 7;
+	periodVarianceMS = 0;
+	ejectionVelocity = 2;
+	velocityVariance = 1.0;
+	ejectionOffset   = 0.0;
+	thetaMin         = 0;
+	thetaMax         = 60;
+	phiReferenceVel  = 0;
+	phiVariance      = 360;
+	overrideAdvances = false;
+	particles = "KingTrigParticle";
+	noHide = 1;
+};
+
+datablock ParticleEmitterNodeData(KingTrigEmitterNode) {
+	timeMultiple = 1;
+};
+
+function buildKingTrigEmitters(%group) {
+	%count = %group.getCount();
+
+	for (%i = 0; %i < %count; %i++) {
+		%obj = %group.getObject(%i);
+		%class = %obj.getClassName();
+
+		if (%class $= "SimGroup")
+			buildKingTrigEmitters(%obj);
+		else if (%class $= "Trigger" &&
+		         %obj.getDataBlock().getName() $= "KingTrigger" &&
+		         !%obj.noEmitters) {
+
+			if (%obj._builtEmitters) {
+				continue;
+			}
+			%obj._builtEmitters = true;
+
+			%pos = %obj.getTransform();
+			%scale = %obj.getScale();
+			%x = getWord(%pos, 0);
+			%y = getWord(%pos, 1);
+			%z = getWord(%pos, 2);
+			%sX = getWord(%scale, 0);
+			%sY = getWord(%scale, 1);
+			%sZ = getWord(%scale, 2);
+
+			%pos[0] = %x SPC %y SPC %z;
+			%pos[1] = %x + %sX SPC %y SPC %z;
+			%pos[2] = %x SPC %y - %sY SPC %z;
+			%pos[3] = %x + %sX SPC %y - %sY SPC %z;
+		
+			%pos[4] = %x SPC %y SPC %z + %sZ;
+			%pos[5] = %x + %sX SPC %y SPC %z + %sZ;
+			%pos[6] = %x SPC %y - %sY SPC %z + %sZ;
+			%pos[7] = %x + %sX SPC %y - %sY SPC %z + %sZ;
+
+			%pos[8] = %x SPC %y - (%sY / 2) SPC %z;
+			%pos[9] = %x + %sX SPC %y - (%sY / 2) SPC %z;
+
+			%pos[10] = %x SPC %y - (%sY / 2) SPC %z + %sZ;
+			%pos[11] = %x + %sX SPC %y - (%sY / 2) SPC %z + %sZ;
+
+			%pos[12] = %x + (%sX / 2) SPC %y SPC %z;
+			%pos[13] = %x + (%sX / 2) SPC %y - %sY SPC %z;
+
+			%pos[14] = %x + (%sX / 2) SPC %y SPC %z + %sZ;
+			%pos[15] = %x + (%sX / 2) SPC %y - %sY SPC %z + %sZ;
+
+			for (%j = 0; %j < 16; %j++) {
+				%staticShape = new StaticShape() {
+					position = %pos[%j];
+					rotation = "1 0 0 0";
+					scale = "1 1 1";
+					datablock = KingTrigEmitterBase;
+				};
+				MissionCleanup.add(%staticShape);
+				$KingTrigStaticShape[%obj.getId()] = %staticShape;
+			}
+		}
+	}
+}
+
 function KingTrigger::onEnterTrigger(%this, %trigger, %obj) {
 	%obj.client.enterKingTrigger(%trigger);
+	messageClient(%obj.client, 'MsgKTrigOne', "You have entered the King Trigger!");
+	%obj.client.playPitchedSound("gotDiamond");
 }
 
 function KingTrigger::onLeaveTrigger(%this, %trigger, %obj) {
 	%obj.client.leaveKingTrigger(%trigger);
+	messageClient(%obj.client, 'MsgKTrigTwo', "You have exited the King Trigger!");
+	%obj.client.playPitchedSound("missinggems");
 }
