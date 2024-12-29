711f52272a8c7ca183ec0b7438213f0f192f3d6f
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2024-12-29 19:10:00 +0530

    minor fixes

--- Marble Blast Platinum/platinum/client/scripts/audioprofiles.cs
+++ Marble Blast Platinum/platinum/client/scripts/audioprofiles.cs
@@ -375,8 +375,8 @@ function pitchMusic() {
 	%targetPitch = 1;
 
 	//Warp Speed Music
-	if ($pref::warpSpeedMusic && $InPlayGUI) {
-		%targetPitch = (mPow(VectorLen(MPGetMyMarble().getVelocity()) / 360, 1/2));
+	if ($pref::warpSpeedMusic && $InPlayGUI && isObject($MP::MyMarble)) {
+		%targetPitch = (mPow(VectorLen($MP::MyMarble.getVelocity()) / 360, 1/2));
 		if (%targetPitch < 1)
 			%targetPitch = 1;
 	}
