e51e5f09397904ed4e8ce8acdfd9f13b3dd5c42f
TheGreatAutismo <yoshicraft224@gmail.com>
2024-12-30 00:18:37 -0500

    Update playGui.cs
    * Fixed bug where the timer would always be green with the par time alarm disabled
    * "x seconds remaining" and "The par time has passed" text is now able to show up again if the par time alarm is disabled and reenabled within the same level

--- Marble Blast Platinum/platinum/client/scripts/playGui.cs
+++ Marble Blast Platinum/platinum/client/scripts/playGui.cs
@@ -785,21 +785,22 @@ function PlayGui::addBonusTime(%this, %dt) {
 }
 
 function PlayGui::refreshRed(%this) {
-
-	//Do not 'refresh red' or play the alarm if the player has disabled the alarm option.
-	if (!$pref::parTimeAlarm)
-		return;
-
 	if ($PlayTimerActive && $InPlayGUI) {
-		if (%this.bonusTime || $Editor::Opened || %this.stopped)
+		if (%this.bonusTime || $Editor::Opened || %this.stopped) {
 			$PlayTimerColor = $TimeColor["stopped"];
-		else {
+		} else if (!$pref::parTimeAlarm) {
+			$PlayTimerColor = $TimeColor["normal"];
+			%this.isAlarmActive  = false;
+			$PlayTimerAlarmText  = false;
+			$PlayTimerFailedText = false;
+		} else {
 			%dir = ClientMode::callback("timeMultiplier", 1);
-			%this.isAlarmActive = false;
 			if      (%dir > 0)
 				%this.isAlarmActive = %this.currentTime >= (MissionInfo.time - $PlayTimerAlarmStartTime) && %this.currentTime < MissionInfo.time;
 			else if (%dir < 0)
 				%this.isAlarmActive = %this.currentTime <=                     $PlayTimerAlarmStartTime  && %this.currentTime > 0;
+			else
+				%this.isAlarmActive = false;
 
 			if (%this.isAlarmActive) {
 				if (!alxIsPlaying($PlayTimerAlarmHandle))
