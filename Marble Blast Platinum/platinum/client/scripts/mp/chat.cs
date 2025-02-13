8dc90d86a5e52ba6f961919abfa48b7a8bcc2717
TheGreatAutismo <yoshicraft224@gmail.com>
2025-02-12 22:27:20 -0500

    Update chat.cs
    * /slap, /boo, and /hohoho no longer send the actual command itself as a message in chat
    * /boo and /hohoho now only work for the server host (UNTESTED)

--- Marble Blast Platinum/platinum/client/scripts/mp/chat.cs
+++ Marble Blast Platinum/platinum/client/scripts/mp/chat.cs
@@ -71,10 +71,19 @@ function onServerChat(%user, %message) {
 			return;
 		case "slap":
 			MPAddServerChat(LBSlapMessage(%user, getWord(%message, 1)));
+			return;
 		case "boo":
-			PlayMissionGui.toggleFrightfest();
+			if ($Server::Hosting)
+				PlayMissionGui.toggleFrightfest();
+			else
+				MPAddServerChat(LBChatColor("admin") @ "You must be the server host to enable Frightfest!");
+			return;
 		case "hohoho":
-			PlayMissionGui.toggleWinterfest();
+			if ($Server::Hosting)
+				PlayMissionGui.toggleWinterfest();
+			else
+				MPAddServerChat(LBChatColor("admin") @ "You must be the server host to enable Winterfest!");
+			return;
 		}
 	}
 
