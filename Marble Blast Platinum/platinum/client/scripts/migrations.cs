0622c90a96fdd5f21a020320ecd65bc79a610d13
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2025-02-19 14:20:33 +0530

    migrate better

--- Marble Blast Platinum/platinum/client/scripts/migrations.cs
+++ Marble Blast Platinum/platinum/client/scripts/migrations.cs
@@ -22,22 +22,91 @@
 
 // 2.11 - Migrate scores from old hunt mission paths to new ones
 
+$Migrations::SourcePath[0] = "platinum/data/multiplayer/hunt/intermediate/RampsRevamped_Hunt.mcs";
+$Migrations::TargetPath[0] = "platinum/data/multiplayer/hunt/advanced/RampsRevamped_Hunt.mcs";
+$Migrations::SourcePath[1] = "platinum/data/multiplayer/hunt/intermediate/Sweep_Hunt.mcs";
+$Migrations::TargetPath[1] = "platinum/data/multiplayer/hunt/advanced/Sweep_Hunt.mcs";
+$Migrations::SourcePath[2] = "platinum/data/multiplayer/hunt/intermediate/TerraceTundra_Hunt.mcs";
+$Migrations::TargetPath[2] = "platinum/data/multiplayer/hunt/advanced/TerraceTundra_Hunt.mcs";
+$Migrations::SourcePath[3] = "platinum/data/multiplayer/hunt/intermediate/Tilo_Hunt.mcs";
+$Migrations::TargetPath[3] = "platinum/data/multiplayer/hunt/advanced/Tilo_Hunt.mcs";
+$Migrations::SourcePath[4] = "platinum/data/multiplayer/hunt/custom/nuked/KingOfTheRing_Hunt.mis";
+$Migrations::TargetPath[4] = "platinum/data/multiplayer/hunt/beginner/KingOfTheRing_Hunt.mis";
+$Migrations::SourcePath[5] = "platinum/data/multiplayer/hunt/custom/existing/archipelago.mis";
+$Migrations::TargetPath[5] = "platinum/data/multiplayer/hunt/bonus/Archipelago_Hunt.mis";
+$Migrations::SourcePath[6] = "platinum/data/multiplayer/hunt/custon/nuked/BlenderChop_Hunt.mcs";
+$Migrations::TargetPath[6] = "platinum/data/multiplayer/hunt/bonus/BlenderChop_Hunt.mcs";
+$Migrations::SourcePath[7] = "platinum/data/multiplayer/hunt/intermediate/Exoplanet_Hunt.mcs";
+$Migrations::TargetPath[7] = "platinum/data/multiplayer/hunt/bonus/Exoplanet_Hunt.mcs";
+$Migrations::SourcePath[8] = "platinum/data/multiplayer/hunt/custom/nuked/FaultLine_Hunt.mcs";
+$Migrations::TargetPath[8] = "platinum/data/multiplayer/hunt/bonus/FaultLine_Hunt.mcs";
+$Migrations::SourcePath[9] = "platinum/data/multiplayer/hunt/custom/existing/GoodToBeKing.mis";
+$Migrations::TargetPath[9] = "platinum/data/multiplayer/hunt/bonus/GoodToBeKing_Hunt.mis";
+$Migrations::SourcePath[10] = "platinum/data/multiplayer/hunt/custom/existing/Grassy.mis";
+$Migrations::TargetPath[10] = "platinum/data/multiplayer/hunt/bonus/Grassy_Hunt.mis";
+$Migrations::SourcePath[11] = "platinum/data/multiplayer/hunt/beginner/GravityTower_Hunt.mcs";
+$Migrations::TargetPath[11] = "platinum/data/multiplayer/hunt/bonus/GravityTower_Hunt.mcs";
+$Migrations::SourcePath[12] = "platinum/data/multiplayer/hunt/custom/existing/Gym_Hunt.mis";
+$Migrations::TargetPath[12] = "platinum/data/multiplayer/hunt/bonus/Gym_Hunt.mis";
+$Migrations::SourcePath[13] = "platinum/data/multiplayer/hunt/custom/existing/Heart_of_the_Typhoon.mis";
+$Migrations::TargetPath[13] = "platinum/data/multiplayer/hunt/bonus/HeartOfTheTyphoon_Hunt.mis";
+$Migrations::SourcePath[14] = "platinum/data/multiplayer/hunt/custom/existing/HikarusVShape.mis";
+$Migrations::TargetPath[14] = "platinum/data/multiplayer/hunt/bonus/HikarusVShape_Hunt.mis";
+$Migrations::SourcePath[15] = "platinum/data/multiplayer/hunt/custom/existing/Landscape.mis";
+$Migrations::TargetPath[15] = "platinum/data/multiplayer/hunt/bonus/Landscape_Hunt.mis";
+$Migrations::SourcePath[16] = "platinum/data/multiplayer/hunt/custom/existing/Layered.mis";
+$Migrations::TargetPath[16] = "platinum/data/multiplayer/hunt/bonus/Layered_Hunt.mis";
+$Migrations::SourcePath[17] = "platinum/data/multiplayer/hunt/custom/existing/Quartile.mis";
+$Migrations::TargetPath[17] = "platinum/data/multiplayer/hunt/bonus/Quartile_Hunt.mis";
+$Migrations::SourcePath[18] = "platinum/data/multiplayer/hunt/custom/nuked/Ring_Hunt.mis";
+$Migrations::TargetPath[18] = "platinum/data/multiplayer/hunt/bonus/Ring_Hunt.mis";
+$Migrations::SourcePath[19] = "platinum/data/multiplayer/hunt/custom/existing/SpherePlates.mis";
+$Migrations::TargetPath[19] = "platinum/data/multiplayer/hunt/bonus/SpherePlates_Hunt.mis";
+$Migrations::SourcePath[20] = "platinum/data/multiplayer/hunt/custom/existing/timbertown.mis";
+$Migrations::TargetPath[20] = "platinum/data/multiplayer/hunt/bonus/Timbertown_Hunt.mis";
+$Migrations::SourcePath[21] = "platinum/data/multiplayer/hunt/custom/existing/TripleDeckers.mis";
+$Migrations::TargetPath[21] = "platinum/data/multiplayer/hunt/bonus/TripleDeckers_Hunt.mis";
+$Migrations::SourcePath[22] = "platinum/data/multiplayer/hunt/advanced/WonkyWaters_Hunt.mcs";
+$Migrations::TargetPath[22] = "platinum/data/multiplayer/hunt/bonus/WonkyWaters_Hunt.mcs";
+$Migrations::SourcePath[23] = "platinum/data/multiplayer/hunt/intermediate/BasicAgilityCourse_Hunt.mcs";
+$Migrations::TargetPath[23] = "platinum/data/multiplayer/hunt/directorscut/BasicAgilityCourse_Hunt.mcs";
+$Migrations::SourcePath[24] = "platinum/data/multiplayer/hunt/custom/existing/Daedalus_Hunt.mis";
+$Migrations::TargetPath[24] = "platinum/data/multiplayer/hunt/directorscut/Daedalus_Hunt.mis";
+$Migrations::SourcePath[25] = "platinum/data/multiplayer/hunt/custom/existing/Meltdown.mis";
+$Migrations::TargetPath[25] = "platinum/data/multiplayer/hunt/directorscut/Meltdown_Hunt.mis";
+$Migrations::SourcePath[26] = "platinum/data/multiplayer/hunt/custom/nuked/RampMatrix_Hunt.mis";
+$Migrations::TargetPath[26] = "platinum/data/multiplayer/hunt/directorscut/RampMatrix_Hunt.mis";
+$Migrations::SourcePath[27] = "platinum/data/multiplayer/hunt/custom/nuked/RampsReloaded_Hunt.mis";
+$Migrations::TargetPath[27] = "platinum/data/multiplayer/hunt/directorscut/RampsReloaded_Hunt.mis";
+$Migrations::SourcePath[28] = "platinum/data/multiplayer/hunt/advanced/ParkourPeaks_Hunt.mcs";
+$Migrations::TargetPath[28] = "platinum/data/multiplayer/hunt/expert/ParkourPeaks_Hunt.mcs";
+$Migrations::SourcePath[29] = "platinum/data/multiplayer/hunt/advanced/PlatinumConstructionCo_Hunt.mcs";
+$Migrations::TargetPath[29] = "platinum/data/multiplayer/hunt/expert/PlatinumConstructionCo_Hunt.mcs";
+$Migrations::SourcePath[30] = "platinum/data/multiplayer/hunt/custom/existing/prophetic.mis";
+$Migrations::TargetPath[30] = "platinum/data/multiplayer/hunt/expert/Prophetic_Hunt.mis";
+$Migrations::SourcePath[31] = "platinum/data/multiplayer/hunt/advanced/Spires_Hunt.mcs";
+$Migrations::TargetPath[31] = "platinum/data/multiplayer/hunt/expert/Spires_Hunt.mcs";
+$Migrations::SourcePath[32] = "platinum/data/multiplayer/hunt/beginner/Apex_Hunt.mcs";
+$Migrations::TargetPath[32] = "platinum/data/multiplayer/hunt/intermediate/Apex_Hunt.mcs";
+$Migrations::SourcePath[33] = "platinum/data/multiplayer/hunt/custom/existing/PointsOfTheRadar.mis";
+$Migrations::TargetPath[33] = "platinum/data/multiplayer/hunt/intermediate/PointsOfTheRadar_Hunt.mis";
+$Migrations::SourcePath[34] = "platinum/data/multiplayer/hunt/advanced/SkateParkSquare_Hunt.mcs";
+$Migrations::TargetPath[34] = "platinum/data/multiplayer/hunt/intermediate/SkateParkSquare_Hunt.mcs";
+$Migrations::Count = 35;
+
 if (!$Pref::Migrated2_11) {
-	%oldMissions = "platinum/data/multiplayer/hunt/intermediate/RampsRevamped_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/intermediate/Sweep_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/intermediate/TerraceTundra_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/intermediate/Tilo_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/custom/nuked/KingOfTheRing_Hunt.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/archipelago.mis" TAB "platinum/data/multiplayer/hunt/custon/nuked/BlenderChop_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/intermediate/Exoplanet_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/custom/nuked/FaultLine_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/custom/existing/GoodToBeKing.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/Grassy.mis" TAB "platinum/data/multiplayer/hunt/beginner/GravityTower_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/custom/existing/Gym_Hunt.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/Heart_of_the_Typhoon.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/HikarusVShape.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/Landscape.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/Layered.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/Quartile.mis" TAB "platinum/data/multiplayer/hunt/custom/nuked/Ring_Hunt.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/SpherePlates.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/timbertown.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/TripleDeckers.mis" TAB "platinum/data/multiplayer/hunt/advanced/WonkyWaters_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/intermediate/BasicAgilityCourse_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/custom/existing/Daedalus_Hunt.mis" TAB "platinum/data/multiplayer/hunt/custom/existing/Meltdown.mis"   TAB "platinum/data/multiplayer/hunt/custom/nuked/RampMatrix_Hunt.mis" TAB "platinum/data/multiplayer/hunt/custom/nuked/RampsReloaded_Hunt.mis" TAB "platinum/data/multiplayer/hunt/advanced/ParkourPeaks_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/advanced/PlatinumConstructionCo_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/custom/existing/prophetic.mis" TAB "platinum/data/multiplayer/hunt/advanced/Spires_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/beginner/Apex_Hunt.mcs"     TAB "platinum/data/multiplayer/hunt/custom/existing/PointsOfTheRadar.mis"   TAB "platinum/data/multiplayer/hunt/advanced/SkateParkSquare_Hunt.mcs";
-	%newMissions = "platinum/data/multiplayer/hunt/advanced/RampsRevamped_Hunt.mcs"     TAB "platinum/data/multiplayer/hunt/advanced/Sweep_Hunt.mcs"     TAB "platinum/data/multiplayer/hunt/advanced/TerraceTundra_Hunt.mcs"     TAB "platinum/data/multiplayer/hunt/advanced/Tilo_Hunt.mcs"     TAB "platinum/data/multiplayer/hunt/beginner/KingOfTheRing_Hunt.mis"     TAB "platinum/data/multiplayer/hunt/bonus/Archipelago_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/BlenderChop_Hunt.mcs"        TAB "platinum/data/multiplayer/hunt/bonus/Exoplanet_Hunt.mcs"        TAB "platinum/data/multiplayer/hunt/bonus/FaultLine_Hunt.mcs"        TAB "platinum/data/multiplayer/hunt/bonus/GoodToBeKing_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/Grassy_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/GravityTower_Hunt.mcs"    TAB "platinum/data/multiplayer/hunt/bonus/Gym_Hunt.mis"           TAB "platinum/data/multiplayer/hunt/bonus/HeartOfTheTyphoon_Hunt.mis"         TAB "platinum/data/multiplayer/hunt/bonus/HikarusVShape_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/Landscape_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/Layered_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/Quartile_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/Ring_Hunt.mis"        TAB "platinum/data/multiplayer/hunt/bonus/SpherePlates_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/Timbertown_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/TripleDeckers_Hunt.mis"      TAB "platinum/data/multiplayer/hunt/bonus/WonkyWaters_Hunt.mcs"    TAB "platinum/data/multiplayer/hunt/directorscut/BasicAgilityCourse_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/directorscut/Daedalus_Hunt.mis"    TAB "platinum/data/multiplayer/hunt/directorscut/Meltdown_Hunt.mis" TAB "platinum/data/multiplayer/hunt/directorscut/RampMatrix_Hunt.mis" TAB "platinum/data/multiplayer/hunt/directorscut/RampsReloaded_Hunt.mis" TAB "platinum/data/multiplayer/hunt/expert/ParkourPeaks_Hunt.mcs"   TAB "platinum/data/multiplayer/hunt/expert/PlatinumConstructionCo_Hunt.mcs"   TAB "platinum/data/multiplayer/hunt/expert/Prophetic_Hunt.mis"     TAB "platinum/data/multiplayer/hunt/expert/Spires_Hunt.mcs"   TAB "platinum/data/multiplayer/hunt/intermediate/Apex_Hunt.mcs" TAB "platinum/data/multiplayer/hunt/intermediate/PointsOfTheRadar_Hunt.mis" TAB "platinum/data/multiplayer/hunt/intermediate/SkateParkSquare_Hunt.mcs";
-
-	%count = getFieldCount(%oldMissions);
-
-	for (%i = 0; %i < %count; %i++) {
-		%oldMission = getField(%oldMissions, %i);
-		if ($pref::highscores[%oldMission, 0]) {
-			%newMission = getField(%newMissions, %i);
+	for (%i = 0; %i < $Migrations::Count; %i++) {
+		%oldMission = $Migrations::SourcePath[%i];
+		if ($pref::highscores[%oldMission, 0] !$= "") {
+			%newMission = $Migrations::TargetPath[%i];
 			echo("Migrating high scores from" SPC %oldMission SPC "to" SPC %newMission);
 			for (%j = 0; %j < 5; %j++) {
 				$pref::highscores[%newMission, %j] = $pref::highscores[%oldMission, %j];
 			}
+			$pref::LevelOOBs[%newMission] = $pref::LevelOOBs[%oldMission];
+			$pref::LevelRespawns[%newMission] = $pref::LevelRespawns[%oldMission];
+			$pref::LevelTimes[%newMission] = $pref::LevelTimes[%oldMission];
 		}
 	}
-
 	$Pref::Migrated2_11 = true;
 }
