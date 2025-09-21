ffe861cb38d3eed7cb0cfeb12f65a6853da61258
TheGreatAutismo <yoshicraft224@gmail.com>
2025-09-21 09:44:26 -0400

    More Rich Presence Text!
    * Super Secret Puzzle 12's name is now obscured
    * The level name no longer disappears while in Spectator Mode
    * Each different multiplayer mode now has its own text instead of just reading "In a Server"
    * The level name is now displayed while watching replays or in the level editor
    * "Single Player" has been replaced with unique messages for:
      * Playing MBG, MBU, MBP, and PQ individually
      * Leaderboards customs
      * Playing multiplayer levels offline
      * Playing levels off of Marbleland
      * Playing the Weekly Challenge
      * and playing locally stored customs
    * "Global Lobby" now switches to "Chatting in Webchat" when chatting in Webchat :)
    * New messages for being in the server browser, being in the Marbleland level select, logging in, and logging out
    * "In a Server Lobby" has been replaced with unique messages for dedicated servers, hosting your own server, and whether or not the server you're on is password-protected
    * Added "Online"/"Offline" text when on the main menu, in webchat, in the server browser, in the level select, logging in, and logging out
    * Also sniped one last PlayMissionGui.marbleland I missed before :^)

--- Marble Blast Platinum/platinum/client/scripts/missiondownload.cs
+++ Marble Blast Platinum/platinum/client/scripts/missiondownload.cs
@@ -77,7 +77,7 @@ function onMissionDownloadPhase1(%missionName, %musicTrack) {
 	//MessageHud.close();
 	//cls();
 
-	if (!$pref::NoFadeIn && (mp() || PlayMissionGui.marbleland)) {
+	if (!$pref::NoFadeIn && (mp() || ml())) {
 		$PlayGuiFader = true; // this variable is used for the fader effect in playGui::onWake
 	}
 
